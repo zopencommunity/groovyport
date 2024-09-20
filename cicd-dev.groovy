6657cb76874cbbd63f56f93d66447b06dd3d9600
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:37:24 -0400

    Change organization to zopencommunity

--- cicd-dev.groovy
+++ cicd-dev.groovy
@@ -6,9 +6,9 @@ node('linux')
       branches: [[name: '*/main']],
       doGenerateSubmoduleConfigurations: false,
       extensions: [],
-      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/groovyport.git']]])
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/groovyport.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/groovyport.git'), string(name: 'PORT_DESCRIPTION', value: 'Apache Groovy is a powerful, optionally typed and dynamic language, with static-typing and static compilation capabilities, for the Java platform aimed at improving developer productivity thanks to a concise, familiar and easy to learn syntax.' ), string(name: 'BUILD_LINE', value: 'DEV') ]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/groovyport.git'), string(name: 'PORT_DESCRIPTION', value: 'Apache Groovy is a powerful, optionally typed and dynamic language, with static-typing and static compilation capabilities, for the Java platform aimed at improving developer productivity thanks to a concise, familiar and easy to learn syntax.' ), string(name: 'BUILD_LINE', value: 'DEV') ]
   }
 }
